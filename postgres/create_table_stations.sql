023ab06861516cea37d3ad088b352c9587dab7e9
Daniel <leibowitzdaniel@gmail.com>
2024-08-25 22:58:02 -0700

    Removed comments from SQL file containing CREATE statement for stations lookup table

--- postgres/create_table_stations.sql
+++ postgres/create_table_stations.sql
@@ -9,22 +9,3 @@ CREATE TABLE climate_viewer.lkp.stations (
 	hcn_crn_flag char(3),
 	wmo_id integer
 )
-
-
-/*
-IV. FORMAT OF "ghcnd-stations.txt"
-
-------------------------------
-Variable   Columns   Type
-------------------------------
-ID            1-11   Character - varchar(20) to be safe
-LATITUDE     13-20   Real - decimal(8, 4)
-LONGITUDE    22-30   Real - decimal(8, 4)
-ELEVATION    32-37   Real - decimal(6, 1)
-STATE        39-40   Character - char(2)
-NAME         42-71   Character - varchar(30)
-GSN FLAG     73-75   Character - char(3)
-HCN/CRN FLAG 77-79   Character - char(3)
-WMO ID       81-85   Character - really seems to be an integer
-------------------------------
-*/
